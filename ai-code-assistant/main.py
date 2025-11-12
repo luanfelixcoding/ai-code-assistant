5bb6259fe595c561bd2430947c56740a0686715b
luanfelixcoding <luanfpcoding@gmail.com>
2025-11-11 23:53:02 -0300

    .

--- ai-code-assistant/main.py
+++
@@ -1,9 +0,0 @@
-# Importando
-from customtkinter import CTk
-from app.app_gui import ChatApp
-
-    
-if __name__ == "__main__":
-    app_root = CTk()
-    chat_app = ChatApp(app_root)
-    app_root.mainloop()
